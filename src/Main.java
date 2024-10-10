3c1a8a4e20fe81f9d8d1dbff8137df6c173c7b3d
mt <mt@gmail.com>
2024-10-10 17:18:02 +0200

    Nowy commit lokalnie

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("LOGOWANIE");
         System.out.println("REjestracja");
         System.out.println("Hello world 15");
+        System.out.println("Nowy commit lokalnie");
     }
 }
